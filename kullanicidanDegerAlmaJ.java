88260c9351063793ec82fb287f015c60ae614a74
Süleyman Toklu <99734183+Tiheli@users.noreply.github.com>
2022-10-22 17:58:18 +0300

    Add files via upload

---
+++ kullanicidanDegerAlmaJ.java
@@ -0,0 +1,27 @@
+package kullanicidanDegerAlma;
+
+import java.util.Scanner;
+
+public class kullanicidanDegerAlmaJ {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println( "isminizi giriniz.." );
+		
+		String isim= scan.nextLine();
+		
+// nextLine-> string nextİnt -> int..
+		
+		System.out.println(" merhaba, {"+ isim +"}");
+		
+		
+		
+/* scanner dahil etmke için bulunan satır seçiliyken
+* ctrl+ shift + o yapıyoruz
+* ve import scanner eklemiş oluyoruz
+*/
+	}
+
+}
